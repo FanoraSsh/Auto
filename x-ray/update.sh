7ad27143f67b18360334e5edc38b2a6f42653436
Fanora Ssh Prem <hesaceza476@gmail.com>
2025-01-16 14:14:15 +0700

    Update update.sh

--- x-ray/update.sh
+++ x-ray/update.sh
@@ -7,10 +7,10 @@ rm /usr/local/sbin/cek-ws
 rm /usr/local/sbin/ins-xray
 rm /usr/local/sbin/add-ws
 rm /usr/local/sbin/trialvmess
-wget -O menu "https://raw.githubusercontent.com/Rerechan02/Example/main/menu/menu.sh" && chmod +x menu
-wget -O cek-tr "https://raw.githubusercontent.com/Rerechan02/Example/main/xray/cek-ws.sh" && chmod +x cek-tr
-wget -O ins-xray "https://raw.githubusercontent.com/Rerechan02/Example/main/xray/ins-xray.sh" && chmod +x ins-xray
-wget -O add-ws "https://raw.githubusercontent.com/Rerechan02/Example/main/xray/add-ws.sh" && chmod +x add-ws
-wget -O trialvmess "https://raw.githubusercontent.com/Rerechan02/Example/main/xray/trialvmess.sh" && chmod +x trialvmess
+wget -O menu "https://raw.githubusercontent.com/FanoraSsh/Example/main/menu/menu.sh" && chmod +x menu
+wget -O cek-tr "https://raw.githubusercontent.com/FanoraSsh/Example/main/xray/cek-ws.sh" && chmod +x cek-tr
+wget -O ins-xray "https://raw.githubusercontent.com/FanoraSsh/Example/main/xray/ins-xray.sh" && chmod +x ins-xray
+wget -O add-ws "https://raw.githubusercontent.com/FanoraSsh/Example/main/xray/add-ws.sh" && chmod +x add-ws
+wget -O trialvmess "https://raw.githubusercontent.com/FanoraSsh/Example/main/xray/trialvmess.sh" && chmod +x trialvmess
 
 ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
