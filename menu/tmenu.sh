cbbdcd506ba54f9cace051aeaf2f8e068e3701ca
Fanora Ssh Prem <hesaceza476@gmail.com>
2025-01-16 14:06:51 +0700

    Update tmenu.sh

--- menu/tmenu.sh
+++ menu/tmenu.sh
@@ -68,7 +68,7 @@ echo -e "\033[1;93m┌───────────────────�
 echo -e "\033[1;93m│                • TROJAN MENU •                  \033[1;93m│"
 echo -e "\e[33m└─────────────────────────────────────────────────┘\033[0m"
 echo -e "\033[1;93m┌─────────────────────────────────────────────────┐\033[0m"
-echo -e "\033[1;93m│\033[0m            ${RED}SCRIPT BY RERECHAN STORE $NC \033[1;93m           │\033[0m"
+echo -e "\033[1;93m│\033[0m            ${RED}SCRIPT BY FANORAPREM STORE $NC \033[1;93m           │\033[0m"
 echo -e "\033[1;93m└─────────────────────────────────────────────────┘\033[0m"
 echo -e " [\e[36m•01\e[0m] Create Trojan1         [\e[36m•06\e[0m] Create Trojan2"
 echo -e " [\e[36m•02\e[0m] Trial Trojan1          [\e[36m•07\e[0m] Trial Trojan2"
